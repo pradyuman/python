dd5c4e77c27af8185fc4cfdc03c47f68f2f5f0e9
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-20 00:04:42 -0500

    Finished cleanup and game (pre-debug state)

--- Dungeon Game/game.py
+++ Dungeon Game/game.py
@@ -1,3 +1,4 @@
+import sys
 from character import Character
 from monsters import Dragon, Goblin, Troll
 
@@ -5,7 +6,7 @@ class Game:
 	#setup the gamne
 	def setup(self):
 		self.player = Character()
-		self monsters = [
+		self.monsters = [
 			Goblin(),
 			Troll(),
 			Dragon()
@@ -25,7 +26,7 @@ class Game:
 	#monster's turn
 	def monsterTurn(self):
 		if self.monster.attack():
-			print("{} is attacking!").format(self.monster))
+			print("{} is attacking!".format(self.monster))
 			
 			if self.player.dodge():
 				print("You dodge {}'s attack! Current HP: {}".format(self.monster, self.player.HP))
@@ -57,26 +58,36 @@ class Game:
 				print("Your attack missed!")
 		elif playerMove == 'R':
 			self.player.rest()
-		elif playerMove == 'Q'
+		elif playerMove == 'Q':
 			sys.exit()
 		else:
 			self.playerTurn()
 		
 	def cleanup(self):
+		if self.monster.HP <= 0:
+			self.player.XP += self.monster.XP
+			print("You killed {}!".format(self.monster))
+			self.monster = self.getNextMonster()
 	
 	#overwrite initializing function	
 	def __init__(self):
 		self.setup()
-		
+		#formatting
+		print("\n" + "="*20)
 		#run monster/player turns till one of them dies
 		while self.player.HP and (self.monster or self.monsters):
 			print(self.player)
 			self.monsterTurn()
+			print("-"*20)
 			self.playerTurn()
 			self.cleanup()
+			print("\n" + "=" * 20)
 		
 		if self.player.HP:
 			print("You win!")
 		elif self.monster or self.monsters:
 			print("You lose!")
 			
+		sys.exit()
+
+Game()
