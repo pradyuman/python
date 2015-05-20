c342802a566b9e350544a1510142f3ee635934e2
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-19 23:58:30 -0500

    Added player turn functionality

--- Dungeon Game/game.py
+++ Dungeon Game/game.py
@@ -12,8 +12,8 @@ class Game:
 		]
 		self.monster = self.getNextMonster()
 		
-	def hit(self, player):
-		player.hitpoints -=1
+	def hit(self, entity, strength):
+		entity.HP -= strength
 	
 	#get the next monster on the monster list
 	def getNextMonster(self):
@@ -22,19 +22,45 @@ class Game:
 		except IndexError:
 			return None
 	
+	#monster's turn
 	def monsterTurn(self):
 		if self.monster.attack():
 			print("{} is attacking!").format(self.monster))
 			
 			if self.player.dodge():
-				print("You dodge {}'s attack! Current HP: {}".format(self.monster, self.player.hitpoints))
+				print("You dodge {}'s attack! Current HP: {}".format(self.monster, self.player.HP))
 			else:
-				hit(self.player)
-				print("You were hit! Remaining HP: {}".format(self.player.hitpoints))
+				hit(self.player, 1)
+				print("You were hit! Remaining HP: {}".format(self.player.HP))
 		else:
 			print("The {} is tired. The monster cannot attack this turn.".format(self.monster))
-					
+	
+	#player's turn				
 	def playerTurn(self):
+		playerMove = input("[A]ttack, [R]est, [Q]uit?").upper()
+		
+		#if player is attacking
+		if playerMove == 'A':
+			print("You're attacking the {}".format(self.monster))
+			
+			if self.player.attack():
+				if self.monster.dodge():
+					print("{} dodged your attack!".format(self.monster))
+				else:
+					if self.player.levelUp():
+						hit(self.monster, 2)
+					else:
+						hit(self.monster, 1)
+					
+					print("You hit {} with your {}!".format(self.monster, self.player.weapon))
+			else:
+				print("Your attack missed!")
+		elif playerMove == 'R':
+			self.player.rest()
+		elif playerMove == 'Q'
+			sys.exit()
+		else:
+			self.playerTurn()
 		
 	def cleanup(self):
 	
