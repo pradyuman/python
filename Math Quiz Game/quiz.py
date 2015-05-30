3a2bb3ad278b04936b74b2e6f08808eaa149e957
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-30 15:40:33 -0500

    started math quiz

---
+++ Math Quiz Game/quiz.py
@@ -0,0 +1,19 @@
+import datetime
+import random
+
+from questions import Add
+from questions import Subtract
+from questions import Multiply
+from questions import Divide
+
+class Quiz:
+	questions = []
+	answers = []
+	
+	def __init(self):
+		
+	def take_quiz(self):
+	
+	def ask(self, questions):
+		
+	def summary(def):
