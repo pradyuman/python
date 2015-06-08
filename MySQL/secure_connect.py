ef55e7cc5c8287846e17f3e49cd59fe4f97faacc
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-06-08 16:44:02 -0500

    Added mysql programs

---
+++ MySQL/secure_connect.py
@@ -0,0 +1,24 @@
+from mysql.connector import MySQLConnection, Error
+from mysql_config import read_db_config
+
+def connect():
+	'''Connect to MySQL database '''
+	db_config = read_db_config()
+	try:
+		print('Connecting to MySQL database...')
+		conn = MySQLConnection(**db_config)
+
+		if conn.is_connected():
+			print('Connection established.')
+		else:
+			print('Connection failed.')
+
+	except Error as error:
+		print(error)
+
+	finally:
+		conn.close()
+		print('Connection closed.')
+
+if __name__ == '__main__':
+	connect()
