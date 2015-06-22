21ba45f0c22ee6081aa739edad375d9c02a0fa77
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-06-22 09:56:28 -0500

    Working on quiz

--- Math Quiz Game/quiz.py
+++ Math Quiz Game/quiz.py
@@ -9,11 +9,20 @@ from questions import Divide
 class Quiz:
 	questions = []
 	answers = []
-	
+
 	def __init(self):
-		
+
 	def take_quiz(self):
-	
+
 	def ask(self, questions):
-		
+
+	def total_correct(self):
+
 	def summary(self):
+		print("You got {} out of {} correct.".format(
+			  self.total_correct(), len(self.questions)
+		))
+
+		print("It took you {} seconds total.".format(
+			 (self.end_time-self.start_time).seconds
+		))
