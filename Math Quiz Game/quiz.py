f264fa2d3f5230eaa543f1330e0c6f510a34e5af
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-06 21:38:54 -0500

    Finished math quiz game

--- Math Quiz Game/quiz.py
+++ Math Quiz Game/quiz.py
@@ -11,12 +11,51 @@ class Quiz:
 	answers = []
 
 	def __init(self):
+		question_types = (Add, Subtract, Multiply, Divide)
+
+		for _ in range(10):
+			num1 = random.randint(1,10)
+			num2 = random.randint(1,10)
+			question = random.choice(question_types)(num1, num2)
+			self.questions.append(questions)
 
 	def take_quiz(self):
+		#start time
+		self.start = datetime.datetime.now()
+		
+		#ask questions
+		for q in self.questions:
+			self.answers.append(self.ask(question))
+		else:
+			self.end = datetime.datetime.now()
+		
+		return self.summary()
 
-	def ask(self, questions):
+	def ask(self, question):
+		correct = False
+
+		#start time
+		start = datetime.datetime.now()
+
+		#get user input
+		answer = input(question.text + ' = ')
+
+		#check answer
+		if answer == str(question.answer):
+			correct = True
+
+		#log end time
+		end = datetime.datetime.now()
+
+		return correct, end - start
 
 	def total_correct(self):
+		total = 0
+		for answer in self.answers:
+			if answer[0]:
+				total += 1
+
+		return total
 
 	def summary(self):
 		print("You got {} out of {} correct.".format(
@@ -26,3 +65,6 @@ class Quiz:
 		print("It took you {} seconds total.".format(
 			 (self.end_time-self.start_time).seconds
 		))
+
+if __name__ == '__main__':
+	Quiz().take_quiz()
