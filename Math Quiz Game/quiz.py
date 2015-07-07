ff29a80fc98872dd2c543d120ea6a1c28a83439c
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-06 22:17:27 -0500

    Fixed bugs in math quiz game

--- Math Quiz Game/quiz.py
+++ Math Quiz Game/quiz.py
@@ -10,43 +10,43 @@ class Quiz:
 	questions = []
 	answers = []
 
-	def __init(self):
+	def __init__(self):
 		question_types = (Add, Subtract, Multiply, Divide)
-
+		
 		for _ in range(10):
 			num1 = random.randint(1,10)
 			num2 = random.randint(1,10)
 			question = random.choice(question_types)(num1, num2)
-			self.questions.append(questions)
+			self.questions.append(question)
 
 	def take_quiz(self):
 		#start time
-		self.start = datetime.datetime.now()
+		self.start_time = datetime.datetime.now()
 		
 		#ask questions
 		for q in self.questions:
-			self.answers.append(self.ask(question))
+			self.answers.append(self.ask(q))
 		else:
-			self.end = datetime.datetime.now()
+			self.end_time = datetime.datetime.now()
 		
 		return self.summary()
 
 	def ask(self, question):
 		correct = False
-
+		
 		#start time
 		start = datetime.datetime.now()
-
+		
 		#get user input
 		answer = input(question.text + ' = ')
-
+		
 		#check answer
 		if answer == str(question.answer):
 			correct = True
-
+		
 		#log end time
 		end = datetime.datetime.now()
-
+		
 		return correct, end - start
 
 	def total_correct(self):
@@ -54,7 +54,7 @@ class Quiz:
 		for answer in self.answers:
 			if answer[0]:
 				total += 1
-
+		
 		return total
 
 	def summary(self):
