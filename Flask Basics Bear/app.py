df3071ee0d4be33cc82ab23ca73a8bf4ad43d5e9
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-08 23:58:38 -0500

    Working on flask basics

--- Flask Basics Bear/app.py
+++ Flask Basics Bear/app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, redirect, url_for
+import json
+
+from flask import Flask, render_template, redirect, url_for, request, make_response
 
 
 app = Flask(__name__)
 
+def get_saved_data():
+	try:
+		data = json.loads(request.cookies.get('character'))
+	except TypeError:
+		data = {}
+	return data
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	data = get_saved_data()
+	return render_template('index.html', saves=data)
 
 @app.route('/save', methods=['POST'])
 def save():
-	return redirect(url_for('index'))
+	response = make_response(redirect(url_for('index')))
+	data = get_saved_data()
+	data.update(dict(request.form.items()))
+	response.set_cookie('character', json.dumps(data))
+	return response
 	
-
 app.run(debug=True, host='0.0.0.0', port=8000)
