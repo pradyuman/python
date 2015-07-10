09ac8d93d3fc9ccce87cd53e4d9f5c05ef5716f6
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-09 19:54:21 -0500

    Having some issues...

--- Flask Basics Bear/app.py
+++ Flask Basics Bear/app.py
@@ -1,7 +1,6 @@
 import json
-
 from flask import Flask, render_template, redirect, url_for, request, make_response
-
+from options import DEFAULTS
 
 app = Flask(__name__)
 
@@ -14,12 +13,19 @@ def get_saved_data():
 
 @app.route('/')
 def index():
-	data = get_saved_data()
-	return render_template('index.html', saves=data)
+	return render_template('index.html', saves=get_saved_data())
+
+@app.route('/builder')
+def builder():
+	return render_template(
+			'builder.html',
+			saves=get_saved_data(),
+			options=DEFAULTS
+	)
 
 @app.route('/save', methods=['POST'])
 def save():
-	response = make_response(redirect(url_for('index')))
+	response = make_response(redirect(url_for('builder')))
 	data = get_saved_data()
 	data.update(dict(request.form.items()))
 	response.set_cookie('character', json.dumps(data))
