f71896d10b92fe9043c537cb892aa830a0a1aa55
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-12 17:38:14 -0500

    Added flash messages

--- Customize-A-Bear/app.py
+++ Customize-A-Bear/app.py
@@ -1,8 +1,10 @@
 import json
-from flask import Flask, render_template, redirect, url_for, request, make_response
+from flask import (Flask, render_template, redirect,
+				   url_for, request, make_response, flash)
 from options import DEFAULTS
 
 app = Flask(__name__)
+app.secret_key = 'feasfsdafuwiofh923rio230nf@#%!$3469p'
 
 def get_saved_data():
 	try:
@@ -25,10 +27,11 @@ def builder():
 
 @app.route('/save', methods=['POST'])
 def save():
+	flash("Saved changes!")
 	response = make_response(redirect(url_for('builder')))
 	data = get_saved_data()
 	data.update(dict(request.form.items()))
 	response.set_cookie('character', json.dumps(data))
 	return response
-	
+
 app.run(debug=True, host='0.0.0.0', port=8000)
