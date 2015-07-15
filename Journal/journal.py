ddb4a0be1895b1f7060a2d3dced18fd8a9db5c25
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-15 18:10:29 -0500

    Created menu for journal application

--- Journal/journal.py
+++ Journal/journal.py
@@ -1,14 +1,40 @@
+#!/usr/bin/env python3
+from collections import OrderedDict
+import datetime
+
 from peewee import *
 
 db = SqliteDatabase('journal.db')
 
 class Entry(Model):
-	
+	content = TextField()
+	timestamp = DateTimeField(default=datetime.datetime.now)
+
 	class Meta:
 		database = db
 
+def initialize():
+	'''Create database and content table if they don't exist.'''
+	db.connect()
+	db.create_tables([Entry], safe=True)
+
 def menu_loop():
 	'''Show the menu'''
+	choice = None
+
+	while choice != 'q':
+		print("Enter 'q' to quit.")
+		
+		#print menu
+		for key, value in menu.items():
+			print('{}) {}'.format(key, value.__doc__))
+		
+		#get user input
+		choice = input('Action: ').lower().strip()
+		
+		#run the function
+		if choice in menu:
+			menu[choice]()
 
 def add_entry():
 	'''Add an entryto the database'''
@@ -19,6 +45,13 @@ def view_entries():
 def delete_entry(entry):
 	'''Delete an entry in the database'''
 
+#OrderedDict of journal functions
+menu = OrderedDict([
+		('a', add_entry),
+		('v', view_entries),
+	   ])
+
 if __name__=='__main__':
+	initialize()
 	menu_loop()
 
