302cededfb665447ffbbc9ea965f2d32df914f02
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-07-18 22:19:18 -0500

    Added deleting functionality and cleaned up code/menu sytem

--- Journal/journal.py
+++ Journal/journal.py
@@ -14,10 +14,12 @@ class Entry(Model):
 	class Meta:
 		database = db
 
+
 def initialize():
 	'''Create database and content table if they don't exist.'''
 	db.connect()
 	db.create_tables([Entry], safe=True)
+
 
 def menu_loop():
 	'''Show the menu'''
@@ -29,14 +31,15 @@ def menu_loop():
 		#print menu
 		for key, value in menu.items():
 			print('{}) {}'.format(key, value.__doc__))
-		print('q) Quit application')
+		print("q) Quit application")
 		
 		#get user input
-		choice = input('Action: ').lower().strip()
+		choice = input("Action: ").lower().strip()
 		
 		#run the function
 		if choice in menu:
 			menu[choice]()
+
 
 def add_entry():
 	'''Add an entry to the database'''
@@ -44,9 +47,10 @@ def add_entry():
 	data = sys.stdin.read().strip()
 
 	if data:
-		if input('Save entry? [Y/n] ').lower() != 'n':
+		if input("Save entry? [Y/n] ").lower() != 'n':
 			Entry.create(content=data)
 			print("Saved successfully!")
+
 
 def view_entries(search_query=None):
 	'''View all entries currently in the database'''
@@ -56,22 +60,32 @@ def view_entries(search_query=None):
 
 	for entry in entries:
 		timestamp = entry.timestamp.strftime('%A %d %B %Y | %I:%M %p')
+		print()
 		print(timestamp)
 		print('='*len(timestamp))
 		print(entry.content)
-		print('n) Next Entry')
-		print('r) Return to main menu')
+		print()
+		print("n) Next Entry")
+		print("d) Delete Entry")
+		print("r) Return to main menu")
 
-		next_action = input('Action [N/r]: ').lower().strip()
+		next_action = input("Action [N/d/r]: ").lower().strip()
 
 		if next_action == 'r':
 			break
+		elif next_action == 'd':
+			delete_entry(entry)
+
+
 def search_entries():
 	'''Search through all your journal entries'''
-	view_entries(input('Search query: '))
+	view_entries(input("Search query: "))
 
 def delete_entry(entry):
 	'''Delete an entry in the database'''
+	if input("Are you sure? [y/N] ").lower() == 'y':
+		entry.delete_instance()
+		print("Entry deleted.")
 
 #OrderedDict of journal functions
 menu = OrderedDict([
